a6b721845e079b05635f02c1ab51f3dfd6f36358
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-06-25 17:03:04 -0300

    Relacionamento nos models

--- src/database/migrations/1593035995854-CreateAppointments.ts
+++ src/database/migrations/1593035995854-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1593035995854
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
