2626ae884f4470f1b93ee1c78e7fb96d41b562e9
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-18 16:19:29 -0300

    refactor: create UserAvatarController and move code from users.routes to UserAvatarController file

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
 import UsersController from '@modules/users/infra/http/controllers/UsersController';
+import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', usersController.create);
@@ -18,14 +17,7 @@ usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-    return response.json(user);
-  },
+  userAvatarController.update,
 );
 
 export default usersRouter;
