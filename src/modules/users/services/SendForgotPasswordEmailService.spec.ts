60d44a2e7107008bced8c805355d8e081a429cb3
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-20 15:03:24 -0300

    refactor: move sut creation to a factory method

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -2,28 +2,40 @@ import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepo
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
 
+interface SutTypes {
+  sut: SendForgotPasswordEmailService;
+  fakeMailProvider: FakeMailProvider;
+  fakeUsersRepository: FakeUsersRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const fakeMailProvider = new FakeMailProvider();
+  const fakeUsersRepository = new FakeUsersRepository();
+  const sut = new SendForgotPasswordEmailService(
+    fakeUsersRepository,
+    fakeMailProvider,
+  );
+  return { sut, fakeMailProvider, fakeUsersRepository };
+};
+
 describe('SendForgotPasswordEmailService', () => {
   it('Should be able to revocer password using email', async () => {
-    const fakeMailProvider = new FakeMailProvider();
+    const { fakeMailProvider, fakeUsersRepository, sut } = makeSut();
     const sendMailSpy = jest.spyOn(fakeMailProvider, 'sendMail');
 
-    const fakeUsersRepository = new FakeUsersRepository();
     const userData = {
       email: 'valid_email@mail.com',
       name: 'valid_name',
       password: 'valid_password',
     };
+
     await fakeUsersRepository.create(userData);
-
-    const sut = new SendForgotPasswordEmailService(
-      fakeUsersRepository,
-      fakeMailProvider,
-    );
-
     await sut.execute({
       email: 'valid@email.com',
     });
 
     expect(sendMailSpy).toHaveReturned();
   });
+
+  it('Should not be able to recover a non existing user password', () => {});
 });
