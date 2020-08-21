120723419f58d2deaeb858a05c3bfc87ba34f8b8
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-21 14:28:37 -0300

    feat: recuperação de senha

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -2,21 +2,30 @@ import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepo
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
 import AppError from '@shared/errors/AppError';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
 
 interface SutTypes {
   sut: SendForgotPasswordEmailService;
   fakeMailProvider: FakeMailProvider;
   fakeUsersRepository: FakeUsersRepository;
+  fakeUserTokensRepository: FakeUserTokensRepository;
 }
 
 const makeSut = (): SutTypes => {
   const fakeMailProvider = new FakeMailProvider();
   const fakeUsersRepository = new FakeUsersRepository();
+  const fakeUserTokensRepository = new FakeUserTokensRepository();
   const sut = new SendForgotPasswordEmailService(
     fakeUsersRepository,
     fakeMailProvider,
+    fakeUserTokensRepository,
   );
-  return { sut, fakeMailProvider, fakeUsersRepository };
+  return {
+    sut,
+    fakeMailProvider,
+    fakeUsersRepository,
+    fakeUserTokensRepository,
+  };
 };
 
 describe('SendForgotPasswordEmailService', () => {
@@ -46,5 +55,24 @@ describe('SendForgotPasswordEmailService', () => {
     });
 
     await expect(promise).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should generate a forgot password token', async () => {
+    const { fakeUserTokensRepository, fakeUsersRepository, sut } = makeSut();
+
+    const generateTokenSpy = jest.spyOn(fakeUserTokensRepository, 'generate');
+
+    const userData = {
+      email: 'valid_email@mail.com',
+      name: 'valid_name',
+      password: 'valid_password',
+    };
+
+    const createdUser = await fakeUsersRepository.create(userData);
+    await sut.execute({
+      email: 'valid_email@mail.com',
+    });
+
+    expect(generateTokenSpy).toHaveBeenCalledWith(createdUser.id);
   });
 });
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -4,6 +4,7 @@ import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import { IusersRepository } from '../repositories/IusersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
   email: string;
@@ -16,14 +17,18 @@ class SendForgotPasswordEmailService {
     private usersRepository: IusersRepository,
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-    const checkIfUserExists = await this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email);
 
-    if (!checkIfUserExists) {
+    if (!user) {
       throw new AppError('User does not exists.');
     }
+
+    await this.userTokensRepository.generate(user.id);
 
     this.mailProvider.sendMail(
       email,
