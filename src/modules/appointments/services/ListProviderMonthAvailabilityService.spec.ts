fa00342129c39419745ed59019ca462add1f622e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-27 12:01:24 -0300

    test: ensure ListProviderAvailabilityService list all month availability of specific provider

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -12,33 +12,23 @@ describe('ListProvidersMonthAvailabilityService', () => {
     );
   });
   it('Should be able to list the month availability from provider', async () => {
-    await fakeAppointmentsRepository.create({
-      provider_id: 'some_id_out_month',
-      date: new Date(2020, 3, 20, 8, 0, 0),
-    });
-    await fakeAppointmentsRepository.create({
-      provider_id: 'some_id',
-      date: new Date(2020, 4, 20, 8, 0, 0),
-    });
-    await fakeAppointmentsRepository.create({
-      provider_id: 'some_id',
-      date: new Date(2020, 4, 20, 10, 0, 0),
-    });
-    await fakeAppointmentsRepository.create({
-      provider_id: 'some_id',
-      date: new Date(2020, 4, 21, 8, 0, 0),
-    });
-
+    for (let i = 8; i < 18; i++) {
+      await fakeAppointmentsRepository.create({
+        provider_id: 'some_id',
+        date: new Date(2020, 4, 20, i, 0, 0),
+      });
+    }
     const availability = await listProviderMonthAvailabilityService.execute({
       provider_id: 'some_id',
       year: 2020,
       month: 5,
     });
-
     expect(availability).toEqual(
       expect.arrayContaining([
+        { day: 19, available: true },
         { day: 20, available: false },
-        { day: 21, available: false },
+        { day: 21, available: true },
+        { day: 22, available: true },
       ]),
     );
   });
