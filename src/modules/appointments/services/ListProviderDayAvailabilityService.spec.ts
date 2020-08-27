26340fc85acfb278436ef72b93eaa1b86d03b400
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-27 14:54:26 -0300

    test: ensure ListProviderDayAvailabilityService not show available hours of provider in the past

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -18,6 +18,9 @@ describe('ListProviderDayAvailabilityService', () => {
         date: new Date(2020, 4, 20, i, 0, 0),
       });
     }
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 7).getTime();
+    });
     const availability = await listProviderDayAvailabilityService.execute({
       provider_id: 'some_id',
       day: 20,
@@ -30,6 +33,37 @@ describe('ListProviderDayAvailabilityService', () => {
         { hour: 9, available: false },
         { hour: 10, available: false },
         { hour: 11, available: true },
+      ]),
+    );
+  });
+
+  it('Should not be able to list the past hour availability from provider', async () => {
+    for (let i = 14; i < 16; i++) {
+      await fakeAppointmentsRepository.create({
+        provider_id: 'some_id',
+        date: new Date(2020, 4, 20, i, 0, 0),
+      });
+    }
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
+    });
+    const availability = await listProviderDayAvailabilityService.execute({
+      provider_id: 'some_id',
+      day: 20,
+      year: 2020,
+      month: 5,
+    });
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 11, available: false },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
