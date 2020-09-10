f98a3d95a01b5be2aa17fcd727ab83eae41659f2
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-09-10 16:09:25 -0300

    refactor: resolvendo problema de agendamentos no mesmo horário

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -5,7 +5,7 @@ import { IFindAllInDayFromProviderDTO } from '../dtos/IFindAllInDayFromProviderD
 
 export default interface IappointmentsRepository {
   create(data: IcreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  findByDate(date: Date, provider_id: string): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProviderDTO,
   ): Promise<Appointment[]>;
